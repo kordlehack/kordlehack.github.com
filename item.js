09f1ef15ce669bbdf50c33dcb66ca21d44029a1e
Eimsepl <kimes7244@gmail.com>
2022-08-27 00:43:53 +0900

    d
    d

--- item.js
+++ item.js
@@ -1,47 +1,75 @@
-function h_time(h,m,s,lh,ls,city) {
-    document.getElementById("city").innerHTML =city;
+let timer;
 
-    var dDt = new Date();
-    var i_month = dDt.getMonth()+1;
-    var i_day = dDt.getDate();
+function h_time(user_min,user_sec, city) {
+    clearInterval(timer);
+    document.getElementById("city").innerHTML = city;
+
+    var tiem_date = new Date();
+    var i_month = tiem_date.getMonth() + 1;
+    var i_day = tiem_date.getDate();
+    var i_hour = tiem_date.getHours();
+
+    var my_hour = i_hour;
+    var my_min = user_min;
+
+    let dateDB = [];
     if(i_month < 10){
-        i_month = "0"+i_month;
+        i_month  = "0"+i_month;
     }
-    if(i_day < 10){
-        i_day = "0"+i_day;
+    if(my_hour < 10){
+        my_hour  = "0"+my_hour;
     }
-    if(h < 10){
-        h  = "0"+h ;
-    }
-    m = parseInt(m) + parseInt(lh);
-    if(m < 10){
-        m = "0"+m;
-    }
-    if(m >= 60)
-    {
-        h++;
-        m=m-60;
+    dateDB[0] = "2022-" + i_month + "-" + i_day + ":" + my_hour + ":" + my_min + ":"+user_sec;
+
+    for (var i = 1; i < 49; i++) {
+        if (my_min + 30 >= 60 && my_min - 30 >=0) {
+            my_hour = parseInt(my_hour) + 1;
+            my_min = parseInt(my_min) - 30;    
+           
+            if(my_hour < 10){
+                my_hour = "0"+my_hour;
+            }
+
+            if(my_hour >= 24){
+                my_hour= "00";
+                i_day++;
+            }
+            dateDB[i] = "2022-" + i_month + "-" + i_day + ":" + my_hour + ":" + my_min + ":"+user_sec;
+            //console.log("홀 :" + my_min);
+        } else {            
+            my_hour = parseInt(my_hour);
+            my_min = parseInt(my_min) + 30;
+            if(my_hour < 10){
+                my_hour = "0"+my_hour;
+            }
+          
+            if(my_hour >= 24){
+                my_hour= "00";
+                i_day++;
+            }
+            dateDB[i] = "2022-" + i_month + "-" + i_day + ":" + my_hour + ":" + my_min + ":00";
+            //console.log("짝 :" + my_min);
+        }
     }
 
-    let dday = new Date("2022-"+i_month+"-"+i_day+":"+h+":"+m+":00").getTime();
-    document.getElementById("count").innerHTML ="불러오는 중...";
+ 
 
-    setInterval(function () {
+    let dday = new Date(dateDB[0]).getTime();
+    let count = 0;
+
+    timer = setInterval(function () {
         const today = new Date().getTime();
         const gap = dday - today;
         const day = Math.floor(gap / (1000 * 60 * 60 * 24));
         const hour = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const min = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
         const sec = Math.floor((gap % (1000 * 60)) / 1000);
-        
-        if(min <=0 && sec <=0){           
-             if((m + 30) >= 60) {
-                h++;
-                m=m-30;
-             }
-             dday = new Date("2022-"+i_month+"-"+i_day+":"+h+":"+m+":00").getTime();
+
+        if (min <= 0 && sec <= 0) {
+            count=count+1;
+            dday = new Date(dateDB[count]).getTime();     
+            console.log(count+"...." +dateDB[count]);      
         }
-         document.getElementById("count").innerHTML = "교역소 갱신까지 " + min + "분 " + sec + "초 남았습니다.";
-    
+        document.getElementById("count").innerHTML = city+" 교역소 갱신까지 " + min + "분 " + sec + "초 남았습니다.";
     }, 1000);
 }
